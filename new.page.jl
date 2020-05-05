e2f35e6843fe77b24e5a9bbe1de59784e76dc2e9
Heptazhou <heptazhou@icloud.com>
2020-05-05 14:24:07 +0000

    Update

--- new.page.jl
+++ new.page.jl
@@ -10,7 +10,7 @@ function uuid4file(ext::String)
         else
             println(pwd(), ">\n", fname)
             open(fname, "w") do io
-                print(io, "---\r\ntitle: 标题\r\ndescription: 描述\r\npermalink: /标题描述/\r\nredirect_from:\r\n- /标题/\r\n- /$id/\r\nuuid: $id\r\n---\r\n\r\n")
+                print(io, "---\ntitle: 标题\ndescription: 描述\npermalink: /标题描述/\nredirect_from:\n- /标题/\n- /$id/\nuuid: $id\n---\n\n")
             end
             return id
         end
