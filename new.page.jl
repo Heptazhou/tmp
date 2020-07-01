4e8dd67e4d4395a0ba67e460f91b00f77bf8d183
Heptazhou <heptazhou@icloud.com>
2020-07-01 21:28:12 +0000

    Update new.page.jl

--- new.page.jl
+++ new.page.jl
@@ -1,29 +1,29 @@
 using UUIDs: uuid4
 
-indexfile = "index2.md"
+const indexfile = "index2.md"
+const newfileext = ".md"
 
 function uuid4file(ext::String)
     while true
         id = string(uuid4())
         fname = id * ext
-        if ispath(fname)
-        else
-            println(pwd(), ">\n", fname)
-            open(fname, "w") do io
-                print(io, "---\ntitle: 标题\ndescription: 描述\npermalink: /标题描述/\nredirect_from:\n- /标题/\n- /$id/\nuuid: $id\n---\n\n")
-            end
-            return id
+        ispath(fname) && continue
+        println(pwd(), ">\n", fname)
+        open(fname, "w") do io
+            print(io, "---\ntitle: 标题\ndescription: 描述\npermalink: /标题描述/\nredirect_from:\n- /标题/\n- /$id/\nuuid: $id\n---\n\n")
         end
+        return id
     end
 end
 
 cd("docs")
 if isfile(indexfile)
-    id = uuid4file(".md")
+    id = uuid4file(newfileext)
     open(indexfile, "a") do io
         println(io, "[]($id/)")
     end
+    @info "完成"
 else
-    println("ERROR")
+    @info "错误"
 end
 readline()
